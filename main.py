445f751c40d1009e7c473d3e3717b289ebab52ba
Matt Barson <matt.barson@gmail.com>
2021-08-14 09:51:14 +0100

    testing raw capture only.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Main.py controls all the sub methods and classes that do the heavy lifting.
 
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+import time
 
 import find
 import strike
@@ -17,22 +18,24 @@ import camera
 
 def main():
     camera = PiCamera()
-    camera.resolution = (1080, 1080)
+    camera.resolution = (1088, 1088)
     camera.framerate = 30
-    rawCapture = PiRGBArray(camera, size=(1080, 1080))
+    rawCapture = PiRGBArray(camera, size=camera.resolution)
 
     # bot = telegram_bot.TelegramBot()
-    detector = find.Detector()
-
+    # detector = find.Detector()
+    s = time.time()
     for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
         image = frame.array
-        squirrels = detector.darknet_detect(image)
-        print(squirrels)
-        if squirrels is not False:
-            # Squirrel is present
-            detector.save_image(squirrels, image, 'positive.jpg')
-            # bot.send_photo(photo_path='positive.jpg')
-            strike.claymore()
+        print(f'{time.time() - s}')
+
+        # squirrels = detector.darknet_detect(image)
+        # print(squirrels)
+        # if squirrels is not False:
+        #     # Squirrel is present
+        #     detector.save_image(squirrels, image, 'positive.jpg')
+        #     # bot.send_photo(photo_path='positive.jpg')
+        #     strike.claymore()
 
         rawCapture.truncate(0)
 
