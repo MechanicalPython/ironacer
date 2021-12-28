7861f5c9be8a3506548e706c06d539fa7be9b603
Matt Barson <matt.barson@gmail.com>
2021-12-28 22:34:02 +0000

    Added motion detection.

--- find.py
+++ find.py
@@ -7,6 +7,7 @@ import os
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
+import time
 
 from models.common import DetectMultiBackend
 from utils.datasets import LoadStreams
@@ -15,8 +16,22 @@ from utils.plots import Annotator, colors
 from utils.torch_utils import select_device
 
 
+def angle_from_center(fov, total_width, object_loc):
+    """
+    Takes fov and image data to work out on what angle the obejct is from the
+    center of the camera.
+    :param fov: in total degrees of vision
+    :param total_width: in pixels, probably 1080.
+    :param object_loc: in pixels
+    :return: angle relative from the center of the camera.
+    """
+    rel_loc = (object_loc - (total_width / 2)) / (total_width / 2)
+    angle = rel_loc * fov / 2
+    return angle
+
+
 # Deprecated - used for static images.
-class Detector:
+class ImageDetector:
     """
     darknet_detect() box gives left, top, width, height. left and top gives the top left coordinate
     for the box. The width and height then allows you to work out the next 3 corners of the box.
@@ -70,34 +85,38 @@ class Detector:
 
 class StreamDetector:
     """Trying to write the detect_stream method as a better implimented class."""
-    def __init__(self, weights='best.pt', source='http://ironacer.local:8000/stream.mjpg', imgsz=(640, 640), conf_thres=0.25):
-        self.weights = weights
-        self.source = str(source)
-        self.imgsz = imgsz  # inference size (height, width)
-        self.conf_thres = conf_thres  # confidence threshold
-        self.iou_thres = 0.45  # NMS IOU threshold
-        self.max_det = 1000  # maximum detections per image
-        self.device = ''  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        self.device = select_device(self.device)
-        self.classes = None  # filter by class: --class 0, or --class 0 2 3
-        self.agnostic_nms = False  # class-agnostic NMS
-        self.nosave = False  # do not save images/videos
-        self.augment = False  # augmented inference
-        self.visualize = False  # visualize features
-        self.line_thickness = 3  # bounding box thickness (pixels)
-        self.hide_labels = False  # hide labels
-        self.hide_conf = False  # hide confidences
-        self.half = False  # use FP16 half-precision inference
-        self.dnn = False  # use OpenCV DNN for ONNX inference
-        self.model = DetectMultiBackend(self.weights, device=self.device, dnn=self.dnn)
-        self.stride, self.names, self.pt, jit, self.onnx, engine = self.model.stride, self.model.names, self.model.pt, self.model.jit, self.model.onnx, self.model.engine
-        self.half &= (self.pt or jit or engine) and self.device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
-        if self.pt or jit:
-            self.model.model.half() if self.half else self.model.model.float()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        self.model.warmup(imgsz=(1, 3, *imgsz), half=self.half)  # warmup
-        self.number_of_frames_without_squirrel = 0  # How many frames in a row can be false before resetting the vid
-        self.vid_writer = None
+    def __init__(self, weights='best.pt', source='http://ironacer.local:8000/stream.mjpg', imgsz=(1280, 1280), conf_thres=0.25, motion_detection_only=False):
+        if motion_detection_only is False:
+            self.weights = weights
+            self.source = str(source)
+            self.imgsz = imgsz  # inference size (height, width)
+            self.conf_thres = conf_thres  # confidence threshold
+            self.iou_thres = 0.45  # NMS IOU threshold
+            self.max_det = 1000  # maximum detections per image
+            self.device = ''  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+            self.device = select_device(self.device)
+            self.classes = None  # filter by class: --class 0, or --class 0 2 3
+            self.agnostic_nms = False  # class-agnostic NMS
+            self.nosave = False  # do not save images/videos
+            self.augment = False  # augmented inference
+            self.visualize = False  # visualize features
+            self.line_thickness = 3  # bounding box thickness (pixels)
+            self.hide_labels = False  # hide labels
+            self.hide_conf = False  # hide confidences
+            self.half = False  # use FP16 half-precision inference
+            self.dnn = False  # use OpenCV DNN for ONNX inference
+            self.model = DetectMultiBackend(self.weights, device=self.device, dnn=self.dnn)
+            self.stride, self.names, self.pt, jit, self.onnx, engine = self.model.stride, self.model.names, self.model.pt, self.model.jit, self.model.onnx, self.model.engine
+            self.half &= (self.pt or jit or engine) and self.device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
+            if self.pt or jit:
+                self.model.model.half() if self.half else self.model.model.float()
+            cudnn.benchmark = True  # set True to speed up constant image size inference
+            self.model.warmup(imgsz=(1, 3, *imgsz), half=self.half)  # warmup
+            self.number_of_frames_without_squirrel = 0  # How many frames in a row can be false before resetting the vid
+            self.vid_writer = None
+        # For motion detector:
+        self.prev_frame = None
+        self.motion_list = [None, None]
 
     def stream(self):
         """Return raw image from the stream"""
@@ -108,12 +127,66 @@ class StreamDetector:
             im /= 255  # 0 - 255 to 0.0 - 1.0
             if len(im.shape) == 3:
                 im = im[None]  # expand for batch dim
-
-            for batch in self.inference(path, im, im0s, vid_cap, s):
-                yield batch
+            yield path, im, im0s, vid_cap, s
+            # for batch in self.inference(path, im, im0s, vid_cap, s):
+            #     yield batch
 
         # If you get to this point, the stream has been dropped.
         raise AssertionError('Stream cannot be connected to.')
+
+    def motion_detector(self, frame, motion_thresh=500):
+        """
+        Saves the current frame if there is significant enough movement from the previous frame.
+        :param motion_thresh:
+        :param frame:
+        :return:
+        """
+        # Based on Webcam Motion Detector from https://www.geeksforgeeks.org/webcam-motion-detector-python/
+        image_path = None
+        if frame is None:
+            return image_path
+
+        motion = 0  # 0 = no motion, 1 = yes motion.
+
+        og_frame = frame
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Converting color image to gray_scale image
+        frame = cv2.GaussianBlur(frame, (21, 21), 0)
+
+        if self.prev_frame is None:  # Init first frame to gray background.
+            self.prev_frame = frame
+            return image_path
+
+        # Difference between previous frame and current frame(which is GaussianBlur)
+        diff_frame = cv2.absdiff(self.prev_frame, frame)
+
+        # If change in between static background and current frame is greater than 30 it will show white color(255)
+        thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
+        thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+        # Finding contour of moving object
+        cnts, _ = cv2.findContours(thresh_frame.copy(),
+                                   cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        bounding_boxes = []
+        for contour in cnts:
+            amount_of_motion = cv2.contourArea(contour)
+            if amount_of_motion < motion_thresh:  # this is the threshold for motion.
+                continue  # go to next contour.
+
+            motion = 1
+            (x, y, w, h) = cv2.boundingRect(contour)
+            bounding_boxes.append(f'{x} {y} {w} {h}')
+
+        if motion == 1:  # Save the image.
+            ext_num = len([i for i in os.listdir('motion_detected/image/') if i.endswith(".jpg")]) + 1
+            image_path = str(f'motion_detected/image/result-{ext_num}.jpg')
+            cv2.imwrite(image_path, og_frame)  # Write image
+            label_path = str(f'motion_detected/label/result-{ext_num}.txt')
+            with open(label_path, 'w') as f:
+                f.write('\n'.join(bounding_boxes))
+
+        self.prev_frame = frame
+        return image_path
 
     @torch.no_grad()
     def inference(self, path, im, im0s, vid_cap, s):
@@ -145,8 +218,7 @@ class StreamDetector:
     def save_labeled(self, det, im0):
         """Should just need a frame and det."""
         save_dir = 'results/'
-        vid_num = len([i for i in os.listdir(save_dir) if i.endswith(".mp4")]) + 1
-        self.current_vid_path = str(f'{save_dir}result-{vid_num}.mp4')
+
         annotator = Annotator(im0, line_width=self.line_thickness, example=str(self.names))
         if len(det):  # There is a squirrel.
             self.number_of_frames_without_squirrel = 10
@@ -175,7 +247,8 @@ class StreamDetector:
                 if isinstance(self.vid_writer, cv2.VideoWriter):  # If a video has been recorded
                     self.vid_writer.release()  # release previous video writer
                     self.vid_writer = None
-                    vid_done = self.current_vid_path
+                    prev_vid_num = len([i for i in os.listdir(save_dir) if i.endswith(".mp4")])
+                    vid_done = str(f'{save_dir}result-{prev_vid_num}.mp4')
         return vid_done
 
     def save_train_data(self, im0, coordinates):
@@ -324,25 +397,49 @@ def detect_stream(weights='best.pt',  # model.pt path(s)
     raise AssertionError('Stream cannot be connected to')
 
 
-def angle_from_center(fov, total_width, object_loc):
-    """
-    Takes fov and image data to work out on what angle the obejct is from the
-    center of the camera.
-    :param fov: in total degrees of vision
-    :param total_width: in pixels, probably 1080.
-    :param object_loc: in pixels
-    :return: angle relative from the center of the camera.
-    """
-    rel_loc = (object_loc - (total_width / 2)) / (total_width / 2)
-    angle = rel_loc * fov / 2
-    return angle
+def motion_detect_img_dir(path='motion_detected/'):
+    # Assigning our static_back to None
+    prev_frame = None
+
+    # Infinite while loop to treat stack of image as video
+    for i in range(1, len([i for i in os.listdir(f'{path}image/') if i.endswith(".jpg")]) + 1):
+        # Reading frame(image) from video
+        frame = cv2.imread(f'{path}image/result-{i}.jpg')
+        labels = open(f'{path}label/result-{i}.txt', 'r').read().split('\n')
+        for label in labels:
+            x, y, w, h = [int(n) for n in label.split(' ')]
+            # making green rectangle around the moving object
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        # cv2.imshow("Gray Frame", gray)
+        # cv2.imshow("Difference Frame", diff_frame)
+        # cv2.imshow("Threshold Frame", thresh_frame)
+
+        cv2.imshow("Motion Box", frame)
+        time.sleep(0.2)
+        key = cv2.waitKey(1)
+        # if q entered whole process will stop
+        if key == ord('q'):
+            break
+
+    # Destroying all the windows
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    d = StreamDetector(weights='best.pt')
-    for i in d.stream():
-        isSquirrel, coords, confidence, vid_path = i
-        print(isSquirrel, coords, confidence, vid_path)
+    motion_detect_img_dir()
+    # d = StreamDetector(motion_detection_only=True)
+    # vid = cv2.VideoCapture('results/result-114.mp4')
+    # while True:
+    #     i, frame = vid.read()
+    #     if frame is None:
+    #         break
+    #     print(d.motion_detector(frame, motion_thresh=1000))
+
+    # d = StreamDetector(weights='best.pt')
+    # for path, im, im0s, vid_cap, s in d.stream():
+    #     isSquirrel, coords, confidence, vid_path = d.inference(path, im, im0s, vid_cap, s)
+        # print(isSquirrel, coords, confidence, vid_path)
 
     # # For detect_stream
     # for i in detect_stream(source='http://ironacer.local:8000/stream.mjpg'):
