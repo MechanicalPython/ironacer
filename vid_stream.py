821e894bbb586e1c0508186eeca0e28b3629feb4
matt <matt.barson@gmail.com>
2022-01-07 12:47:15 +0000

    Update vid_stream

--- vid_stream.py
+++ vid_stream.py
@@ -3,20 +3,26 @@ Streams the camera video via cv2 to get higher resolution images.
 """
 
 import cv2
+import sys
 
 
-cap = cv2.VideoCapture(0)
+def video_stream(og_res, crop_xywh):
+    cap = cv2.VideoCapture(0)
 
-if not cap.isOpened():
-    raise Exception("Could not open video device")
-# Set properties. Each returns === True on success (i.e. correct resolution)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 3289)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2464)
-# Read picture. ret === True on success
+    if not cap.isOpened():
+        raise Exception("Could not open video device")
+    # Set properties. Each returns === True on success (i.e. correct resolution)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, og_res[0])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, og_res[1])
+    x, y, w, h = crop_xywh
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            frame = frame[y:y+h, x:x+w]
+            sys.stdout.write(frame.tobytes())
+    cap.release()
 
-ret, frame = cap.read()
-cv2.imwrite('cvframe', frame)
 
-# Close device
-cap.release()
+if __name__ == '__main__':
+    video_stream()
 
