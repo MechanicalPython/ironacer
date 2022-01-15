b759138d1e6e32a3194967dd312042059cbec963
matt <matt.barson@gmail.com>
2022-01-15 16:07:05 +0000

    Added show_motion detection method, split saving image and detecting motion apart.

--- pi_motion_detection.py
+++ pi_motion_detection.py
@@ -1,5 +1,5 @@
 """
-Wholesale ripped all from yolov5 datasets and supporting docs to just make this work.
+Script that can be run on the pi to detect motion for capturing data for yolo training.
 
 """
 
@@ -9,19 +9,25 @@ import time
 
 import cv2
 
+# todo - needs to reset the brightness every now and then.
+
 
 class PiMotion:
-    def __init__(self, width, height, imsiz):
+    def __init__(self, width, height, imsiz, on_mac=False, save_images=True):
         self.prev_frame = None
         self.width = width
         self.height = height
         self.imsiz = imsiz
-        self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+        if on_mac:
+            self.cap = cv2.VideoCapture(0)
+        else:
+            self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
         x = (self.width - self.imsiz) / 2
         y = (self.height - self.imsiz) / 2
         self.crop_xywh = (int(x), int(y), self.imsiz, self.imsiz)
+        self.save_images = save_images
 
     def stream(self):
         try:
@@ -34,6 +40,41 @@ class PiMotion:
         finally:
             self.cap.release()
 
+    def _show_motion_live(self):
+        for frame in d.stream():
+            frame, bounding_box = self.motion_detector(frame)
+            if frame is None:
+                continue
+            for label in bounding_box:
+                x, y, w, h, amount_of_motion = label.split(' ')
+                x, y, w, h, amount_of_motion = int(x), int(y), int(w), int(h), str(amount_of_motion)
+                # making green rectangle around the moving object
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                cv2.putText(frame, amount_of_motion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+
+            # cv2.imshow("Gray Frame", gray)
+            # cv2.imshow("Difference Frame", diff_frame)
+            # cv2.imshow("Threshold Frame", thresh_frame)
+
+            cv2.imshow("Motion Box", frame)
+            key = cv2.waitKey(1)
+            # if q entered whole process will stop
+            if key == ord('q'):
+                break
+
+    def save_motion_image(self, frame, bounding_boxes):
+        """Save the motion detected image. """
+        t = str(time.time())
+        parent_folder = os.path.dirname(__file__)
+        if parent_folder == '':
+            parent_folder = '.'
+        print(parent_folder)
+        image_path = f'{parent_folder}/motion_detected/image/result-{t}.jpg'
+        cv2.imwrite(image_path, frame)  # Write image
+        label_path = f'{parent_folder}/motion_detected/label/result-{t}.txt'
+        with open(label_path, 'w') as f:
+            f.write('\n'.join(bounding_boxes))
+
     def motion_detector(self, frame, motion_thresh=500):
         """
         If there is motion between frame A and frame B, this saves frame B and the bounding boxes for that motion
@@ -45,9 +86,8 @@ class PiMotion:
         :param frame:
         :return:
         """
-        image_path = None
         if frame is None:
-            return image_path
+            return None, None
 
         motion = 0  # 0 = no motion, 1 = yes motion.
 
@@ -57,7 +97,7 @@ class PiMotion:
 
         if self.prev_frame is None:  # Init first frame to gray background.
             self.prev_frame = frame
-            return image_path
+            return None, None
 
         # Difference between previous frame and current frame(which is GaussianBlur)
         diff_frame = cv2.absdiff(self.prev_frame, frame)
@@ -80,16 +120,11 @@ class PiMotion:
             (x, y, w, h) = cv2.boundingRect(contour)
             bounding_boxes.append(f'{x} {y} {w} {h} {amount_of_motion}')
 
-        if motion == 1:  # Save the image.
-            t = str(time.time())
-            image_path = f'{os.path.dirname(__file__)}/motion_detected/image/result-{t}.jpg'
-            cv2.imwrite(image_path, og_frame)  # Write image
-            label_path = f'{os.path.dirname(__file__)}/motion_detected/label/result-{t}.txt'
-            with open(label_path, 'w') as f:
-                f.write('\n'.join(bounding_boxes))
+        if motion == 1 and self.save_images:  # Save the image.
+            self.save_motion_image(og_frame, bounding_boxes)
 
         self.prev_frame = frame
-        return image_path
+        return og_frame, bounding_boxes
 
     @staticmethod
     def crop_frame(frame, crop_xywh):
@@ -104,11 +139,12 @@ def arg_parse():
     parser.add_argument('--width', type=int, default=2592)
     parser.add_argument('--height', type=int, default=1944)
     parser.add_argument('--imsiz', type=int, default=1280)
+    parser.add_argument('--on_mac', type=bool, default=False)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     opt = arg_parse()
-    d = PiMotion(opt.width, opt.height, opt.imsiz)
+    d = PiMotion(opt.width, opt.height, opt.imsiz, opt.on_mac)
     for frame in d.stream():
         d.motion_detector(frame)
