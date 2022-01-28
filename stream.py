2ce07756030f1006fa7e0039a6c1572d62b76c60
matt <matt.barson@gmail.com>
2022-01-28 21:59:15 +0000

    Stream as a class

--- stream.py
+++ stream.py
@@ -4,42 +4,7 @@ Stream raw cv2 video, as an array, that other aspects of the program can plug in
 """
 
 import cv2
-import argparse
 import time
-
-
-class Streamer:
-    def __init__(self, width, height, imsiz, on_mac=False):
-        self.reset_freq = 5*60  # Frequency to reset the camera (in seconds).
-        self.width = width
-        self.height = height
-        self.imsiz = imsiz
-        self.on_mac = on_mac
-
-        self.set_video()  # All parameters for the video are to go in this.
-
-    def set_video(self):
-        if self.on_mac:
-            self.cap = cv2.VideoCapture(0)
-        else:
-            self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
-
-    def get_frame(self):
-        t = time.time()
-        try:
-            while True:
-                if time.time() - t > self.reset_freq:
-                    self.set_video()
-                    t = time.time()
-                if self.cap.isOpened():
-                    ret, frame = self.cap.read()
-                    if ret:
-                        frame = cv2.resize(frame, (self.width, self.height))
-                        yield frame
-        finally:
-            self.cap.release()
 
 
 def show_frame(frame, rects=None):
@@ -64,15 +29,67 @@ def show_frame(frame, rects=None):
         return False
 
 
+class LoadWebcam:
+    """
+    Taken and modified from yolov5/utils LoadWebcam.
+    Returns just the image, the augmentation needed for inference is done by find.py.
+    """
+
+    def __init__(self, pipe='0', img_size=1280, stride=32, on_mac=True):
+        self.img_size = img_size
+        self.stride = stride
+        self.pipe = eval(pipe) if pipe.isnumeric() else pipe
+        self.on_mac = on_mac
+        # self.set_camera()
+        self.reset_freq = 5*60  # Frequency to reset the camera (in seconds).
+        self.t = time.time()
+
+    def set_camera(self):
+        if self.on_mac:
+            self.cap = cv2.VideoCapture(0)
+        else:
+            self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.img_size)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.img_size)
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)  # set buffer size
+
+    def __enter__(self):
+        print('start camera')
+        self.set_camera()
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        print('release')
+        self.cap.release()
+
+    def __iter__(self):
+        self.count = -1
+        return self
+
+    def __next__(self):
+        self.count += 1
+        # Read frame
+        ret_val, img = self.cap.read()
+        if time.time() - self.t > self.reset_freq:
+            self.reset_camera()
+            self.t = time.time()
+        return img
+
+    def reset_camera(self):
+        self.cap.release()
+        self.set_camera()
+
+
 # max - 3280 × 2464 pixels
 # 1-15 fps - 2592 x 1944
 
 
 if __name__ == '__main__':
-    stream = Streamer(width=2592, height=1944, imsiz=1280)
-    while True:
-        frame = stream.stream()
-        show_frame(frame)
+    with LoadWebcam() as stream:
+        for img in stream:
+            show_frame(img)
+
 
 
 
