83f098396b7acba262b1d108a41cf3fe1d967c52
matt <matt.barson@gmail.com>
2022-01-30 12:48:16 +0000

    Alter stream for diagnosis.

--- stream.py
+++ stream.py
@@ -109,6 +109,7 @@ if __name__ == '__main__':
     bot = telegram_bot.TelegramBot()
     with LoadWebcam(on_mac=False) as stream:
         for img in stream:
+            print(len(img))
             if stream is None:
                 print('None')
                 time.sleep(1)
