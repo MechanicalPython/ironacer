3797ac6b4c6c1f23b66a3641f53ae85ee67f7934
Matt Barson <matt.barson@gmail.com>
2022-02-07 21:21:22 +0000

    Added updater.sh file to auto update ironacer via cronjob.

--- updater.sh
+++ updater.sh
@@ -4,7 +4,7 @@ cd /home/pi/ironacer
 git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
     git pull
-    reboot;
+    sudo reboot;
 else
     echo "Up-to-date"
 fi
