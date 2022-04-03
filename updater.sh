7a2ea1a032d1655a9286e896dbed60d9be00b129
matt <matt.barson@gmail.com>
2022-04-03 10:57:38 +0100

    Updater.sh always git pull and restart service. To be run at 1am via cronjob.

--- updater.sh
+++ updater.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
 cd /home/pi/ironacer
-git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
-if [ $changed = 1 ]; then
-    git pull
-    sudo reboot;
-else
-    echo "Up-to-date"
-fi
+git pull origin main
+sudo systemctl restart ironacer
